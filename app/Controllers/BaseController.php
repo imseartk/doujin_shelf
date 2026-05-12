d1d0001318081c22e63c422a6dd9934f1babe95a
imseartk <imseartk@gmail.com>
2026-05-12 18:11:08 +0800

    Clean up base controller scaffold

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -3,16 +3,12 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
-use CodeIgniter\HTTP\CLIRequest;
-use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use Psr\Log\LoggerInterface;
 
 abstract class BaseController extends Controller
 {
-    protected $request;
-
     protected $helpers = [];
 
     public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger): void
