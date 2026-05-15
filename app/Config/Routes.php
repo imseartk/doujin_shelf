d6905fcc71fbbb78417fc6266622feabb49cfd16
imseartk <imseartk@gmail.com>
2026-05-15 12:02:39 +0800

    Add taxonomy AJAX routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,6 +7,8 @@ $routes->get('/', 'Books::index');
 
 $routes->get('books', 'Books::index');
 $routes->get('books/new', 'Books::new');
+$routes->get('books/taxonomy/(:segment)/search', 'Books::taxonomySearch/$1');
+$routes->post('books/taxonomy/(:segment)', 'Books::taxonomyStore/$1');
 $routes->post('books', 'Books::create');
 $routes->get('books/(:num)/edit', 'Books::edit/$1');
 $routes->post('books/(:num)', 'Books::update/$1');
