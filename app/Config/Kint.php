a95ee9cf0188b96f41bebe7d0aca1f36acfe02e0
imseartk <imseartk@gmail.com>
2026-05-15 11:06:28 +0800

    Add Kint config

---
+++ app/Config/Kint.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Config;
+
+use Kint\Parser\ConstructablePluginInterface;
+use Kint\Renderer\Rich\TabPluginInterface;
+use Kint\Renderer\Rich\ValuePluginInterface;
+
+class Kint
+{
+    /** @var list<class-string<ConstructablePluginInterface>|ConstructablePluginInterface>|null */
+    public $plugins;
+
+    public int $maxDepth = 6;
+    public bool $displayCalledFrom = true;
+    public bool $expanded = false;
+    public string $richTheme = 'aante-light.css';
+    public bool $richFolder = false;
+
+    /** @var array<string, class-string<ValuePluginInterface>>|null */
+    public $richObjectPlugins;
+
+    /** @var array<string, class-string<TabPluginInterface>>|null */
+    public $richTabPlugins;
+
+    public bool $cliColors = true;
+    public bool $cliForceUTF8 = false;
+    public bool $cliDetectWidth = true;
+    public int $cliMinWidth = 40;
+}
