0895cb8187d8ee2aa166507b769448a9447b44e6
imseartk <imseartk@gmail.com>
2026-05-15 09:21:29 +0800

    Add application routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,4 +3,21 @@
 use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Books::index');
+
+$routes->get('books', 'Books::index');
+$routes->get('books/new', 'Books::new');
+$routes->post('books', 'Books::create');
+$routes->get('books/(:num)/edit', 'Books::edit/$1');
+$routes->post('books/(:num)', 'Books::update/$1');
+$routes->post('books/(:num)/delete', 'Books::delete/$1');
+
+$routes->get('sources', 'Sources::index');
+
+$routes->get('shops', 'Shops::index');
+$routes->post('shops', 'Shops::create');
+$routes->post('shops/(:num)/delete', 'Shops::delete/$1');
+
+$routes->get('locations', 'Locations::index');
+$routes->post('locations', 'Locations::create');
+$routes->post('locations/(:num)/delete', 'Locations::delete/$1');
